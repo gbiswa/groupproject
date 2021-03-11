41ff08d852ca1543c4dd22fb9d001c9364acfa84
gbiswa <71508499+gbiswa@users.noreply.github.com>
2021-03-11 15:46:04 -0600

    upload files

---
+++ store/models/customer.py
@@ -0,0 +1,31 @@
+from django.db import  models
+from django.core.validators import MinLengthValidator
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    password = models.CharField(max_length=500)
+
+    @staticmethod
+    def get_customers_by_id(ids):
+        return Customer.objects.filter(id__in=ids)
+    def register(self):
+        self.save()
+
+    @staticmethod
+    def get_customer_by_email(email):
+        try:
+            return Customer.objects.get(email=email)
+        except:
+            return False
+
+
+    def isExists(self):
+        if Customer.objects.filter(email = self.email):
+            return True
+
+        return  False
+
+
---
+++ store/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from .views.home import Index , store
+from .views.signup import Signup
+from .views.login import Login , logout
+
+
+urlpatterns = [
+    path('', Index.as_view(), name='homepage'),
+    path('store', store , name='store'),
+
+    path('signup', Signup.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login'),
+    path('logout', logout , name='logout'),
+    ]
